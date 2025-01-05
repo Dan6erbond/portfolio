d7345b068ab56fd6f1ca26a54debe71dd8d34905
RaviAnand Mohabir <moravrav@gmail.com>
2025-01-06 00:45:15 +0100

    fix: :bug: suspend Payload admin ui

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,13 +1,14 @@
+import '@payloadcms/next/css'
+import './custom.scss'
+
+import React, { Suspense } from 'react'
+import { RootLayout, handleServerFunctions } from '@payloadcms/next/layouts'
+
+import type { ServerFunctionClient } from 'payload'
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
 import config from '@payload-config'
-import '@payloadcms/next/css'
-import type { ServerFunctionClient } from 'payload'
-import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
-import React from 'react'
-
 import { importMap } from './admin/importMap.js'
-import './custom.scss'
 
 type Args = {
   children: React.ReactNode
@@ -23,9 +24,11 @@ const serverFunction: ServerFunctionClient = async function (args) {
 }
 
 const Layout = ({ children }: Args) => (
-  <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
-    {children}
-  </RootLayout>
+  <Suspense>
+    <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
+      {children}
+    </RootLayout>
+  </Suspense>
 )
 
 export default Layout
