ad041075ea710f30d6772de11ddcbfae88c6cc5e
RaviAnand Mohabir <moravrav@gmail.com>
2025-01-06 10:54:32 +0100

    feat: :sparkles: replace suspense with loading.tsx

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,14 +1,15 @@
 import '@payloadcms/next/css'
 import './custom.scss'
 
-import React, { Suspense } from 'react'
 import { RootLayout, handleServerFunctions } from '@payloadcms/next/layouts'
 
+import React from 'react'
 import type { ServerFunctionClient } from 'payload'
-/* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
-/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
 import config from '@payload-config'
 import { importMap } from './admin/importMap.js'
+
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
 
 type Args = {
   children: React.ReactNode
@@ -24,11 +25,9 @@ const serverFunction: ServerFunctionClient = async function (args) {
 }
 
 const Layout = ({ children }: Args) => (
-  <Suspense>
-    <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
-      {children}
-    </RootLayout>
-  </Suspense>
+  <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
+    {children}
+  </RootLayout>
 )
 
 export default Layout
