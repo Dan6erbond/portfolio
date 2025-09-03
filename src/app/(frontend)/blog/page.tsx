b46b35f5e4a5c139723b2c2c39969c39441d7157
RaviAnand Mohabir <moravrav@gmail.com>
2025-09-03 19:23:23 +0200

    fix: :label: use non-null assertion as tag is required in schema

--- src/app/(frontend)/blog/page.tsx
+++ src/app/(frontend)/blog/page.tsx
@@ -67,7 +67,7 @@ export default async function Blog({
           </Tag>
         ))}
         {tags
-          .filter(({ tag }) => !searchTags.includes(tag))
+          .filter(({ tag }) => !searchTags.includes(tag!))
           .map(({ tag }) => (
             <Tag key={tag} asChild>
               <Link
